d47a4ee15f72189df160ff081d705b112b398379
Diogo Garbinato <diogogarbinato@gmail.com>
2024-07-14 09:24:33 -0300

    feat: tests for participants repository

--- src/models/repositories/participants_repository_test.py
+++ src/models/repositories/participants_repository_test.py
@@ -5,11 +5,11 @@ from src.models.settings.db_connection_handler import db_connection_handler
 
 db_connection_handler.connect()
 participant_id = str(uuid.uuid4())
-#trip_id = str(uuid.uuid4())
-trip_id = "6a74b30a-f209-4a26-8f9d-afcc041776cb"
+trip_id = str(uuid.uuid4())
+#trip_id = "6a74b30a-f209-4a26-8f9d-afcc041776cb"
 emails_to_invite_id = str(uuid.uuid4())
 
-@pytest.mark.skip(reason="interacao com o banco")
+#@pytest.mark.skip(reason="interacao com o banco")
 def test_registry_participant():
     conn = db_connection_handler.get_connection()
     participants_repository = ParticipantsRepository(conn)
@@ -23,7 +23,7 @@ def test_registry_participant():
 
     participants_repository.registry_participant(participant_infos)
 
-@pytest.mark.skip(reason="interacao com o banco")
+#@pytest.mark.skip(reason="interacao com o banco")
 def test_find_participants_from_trip():
     conn = db_connection_handler.get_connection()
     participants_repository = ParticipantsRepository(conn)
